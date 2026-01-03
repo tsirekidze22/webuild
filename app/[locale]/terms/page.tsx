1acf882f4c4fa3c8f1f187dd1084dfc3614c4c91
Tsotne Tsirekidze <90200590+tsirekidze22@users.noreply.github.com>
2026-01-03 14:45:35 +0400

    links updated for correct redirection

--- app/[locale]/terms/page.tsx
+++ app/[locale]/terms/page.tsx
@@ -1,5 +1,5 @@
 import { useTranslations } from "next-intl";
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 
 export default function Terms() {
   const t = useTranslations("terms");
