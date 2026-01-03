3bc5ff9e424498248b98618a128a9a48cf0d0996
Tsotne Tsirekidze <90200590+tsirekidze22@users.noreply.github.com>
2026-01-03 14:43:17 +0400

    copy update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useTranslations } from "next-intl";
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 import LanguageSwitcher from "./reusable/LanguageSwitcher";
 
 export default function Header() {
